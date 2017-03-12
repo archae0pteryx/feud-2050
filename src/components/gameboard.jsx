ef05004a293e6d9e11b42d3c89ff006ec25ca79a
archae0pteryx <>
2017-03-12 12:21:14 -0700

    setting state and plowing vals

--- src/components/gameboard.jsx
+++ src/components/gameboard.jsx
@@ -1,18 +1,41 @@
 import React from 'react'
+// import {db} from '../fire'
 import '../assets/css/gameboard.css'
 export default class extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      value: ''
+    }
+  }
+  handleChange(e) {
+    this.setState({
+      value: e.target.value
+    })
+  }
   render() {
     return (
       <div className="container">
+        <div className="container">
+          <div className="form-group">
+          <input 
+            className="form-control" 
+            placeholder="test"
+            onChange={this.handleChange.bind(this)}
+            value={this.state.value}
+            />
+          <button className="btn btn-primary float-right">push</button>
+          </div>
+        </div>
       <div className="row">
         <div className="col">
           <div className="cell">
-            <h3>A cell</h3>
+            <h3>{this.state.value}</h3>
           </div>
         </div>
         <div className="col">
           <div className="cell">
-            <h3>A cell</h3>
+            <h3>?</h3>
           </div>
         </div>
       </div>
--- src/fire.jsx
+++ src/fire.jsx
@@ -1,7 +1,10 @@
-export const config = {
+import firebase from 'firebase'
+const config = {
   apiKey: "AIzaSyC3IlHRTT8zp8-hsrEoB7egWw7KAi9Q7bs",
   authDomain: "family-feud-2050.firebaseapp.com",
   databaseURL: "https://family-feud-2050.firebaseio.com",
   storageBucket: "family-feud-2050.appspot.com",
   messagingSenderId: "943487803260"
 }
+const fb = firebase.initializeApp(config)
+export const db = fb.database()
