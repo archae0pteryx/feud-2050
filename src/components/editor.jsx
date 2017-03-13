78181d6ba8613cc9d9444ef0b8c519fa9eaa5318
archae0pteryx <>
2017-03-12 17:22:33 -0700

    yarns tart

--- src/components/editor.jsx
+++ src/components/editor.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Questions from './questions'
 import { db } from '../fire'
+import '../assets/css/editor.css'
 
 export default class extends React.Component {
   constructor(props) {
@@ -43,14 +44,22 @@ export default class extends React.Component {
   render() {
     return (
       <div className="container padd">
-        <div className="input-group">
-          <input
-            className="form-control"
-            onChange={this.handleChange.bind(this)}
-            value={this.state.text}
-          />
-          <button onClick={this.pushToBase.bind(this)} className="btn btn-primary">push</button>
+        <div className="adder form-group">
+          <div className="input-group">
+            <span className="labels"><h4>Question </h4></span>
+            <input
+              className="form-control"
+              onChange={this.handleChange.bind(this)}
+              value={this.state.text}
+            />
+          </div>
+          <div className="input-group answer-group">
+            <span className="labels"><h4>Answer </h4></span>
+            <input className="form-control"/>
+            <button className="btn btn-primary">add</button>
+          </div>
         </div>
+            <button onClick={this.pushToBase.bind(this)} className="btn btn-primary">push</button>
         <div className="container padd">
           <Questions questions={ this.state.questions }/>
         </div>
