789842cfe591901c2bc383e76f1097321b941a96
archae0pteryx <>
2017-03-19 14:59:19 -0700

    refactoring rounds and containers

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -8,7 +8,6 @@ export default class extends React.Component {
         <ul className="bottom-nav">
           <li><IndexLink to="/">intro</IndexLink></li>
           <li><Link to="board">board</Link></li>
-          <li><Link to="edit">edit</Link></li>
           <li><Link to="controller">control</Link></li>
         </ul>
       </div>
--- src/containers/gameboard.jsx
+++ src/containers/gameboard.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Square from './square'
 import '../assets/css/gameboard.css'
 export default class extends React.Component {
   componentWillMount() {
@@ -23,11 +22,6 @@ export default class extends React.Component {
     this.setState({round: this.state.round + 1})
   }
   render() {
-    const answer1 = 'sample answer'
-    const answer2 = 'sample answer 2'
-    const answer3 = 'sample answer 3'
-    const answer4 = 'sample answer 4'
-    const answer5 = 'sample answer 5'
     return (
       <div className="container-fluid">
         <div className="round">
@@ -40,21 +34,6 @@ export default class extends React.Component {
             id="next"
             className="fa fa-angle-right"/>
           </div>
-        <div className="row">
-          <Square answer={answer1} clicked={false} cellNum={1} surveyNum={666}/>
-        </div>
-        <div className="row">
-          <Square answer={answer2} clicked={false} cellNum={2} surveyNum={665}/>
-        </div>
-        <div className="row">
-          <Square answer={answer3} clicked={false} cellNum={3} surveyNum={664}/>
-        </div>
-        <div className="row">
-          <Square answer={answer4} clicked={false} cellNum={4} surveyNum={663}/>
-        </div>
-        <div className="row">
-          <Square answer={answer5} clicked={false} cellNum={5} surveyNum={662}/>
-        </div>
       </div>
     )
   }
