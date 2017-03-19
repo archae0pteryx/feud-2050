078e645dbc6123a636fec0d01d6c05dc0a0ba608
archae0pteryx <>
2017-03-19 14:30:48 -0700

    build and new fb domain

--- src/fire.jsx
+++ src/fire.jsx
@@ -1,10 +1,10 @@
 import firebase from 'firebase'
 const config = {
-  apiKey: "AIzaSyC3IlHRTT8zp8-hsrEoB7egWw7KAi9Q7bs",
-  authDomain: "family-feud-2050.firebaseapp.com",
-  databaseURL: "https://family-feud-2050.firebaseio.com",
-  storageBucket: "family-feud-2050.appspot.com",
-  messagingSenderId: "943487803260"
+    apiKey: "AIzaSyA5jTxM2k_oRwgioSaCm1twwgotoGU686A",
+    authDomain: "ff2050-45dca.firebaseapp.com",
+    databaseURL: "https://ff2050-45dca.firebaseio.com",
+    storageBucket: "ff2050-45dca.appspot.com",
+    messagingSenderId: "171386322870"
 }
 const fb = firebase.initializeApp(config)
 export const db = fb.database()
