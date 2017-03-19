07e169a1679cd67c481ca57739a97c775e03503e
archae0pteryx <>
2017-03-19 14:23:49 -0700

    added forward and back round button

--- src/components/new-gameboard.jsx
+++ src/components/new-gameboard.jsx
@@ -3,7 +3,24 @@ import Square from './square'
 import '../assets/css/gameboard.css'
 export default class extends React.Component {
   componentWillMount() {
-    this.setState({round: "Round 1"})
+    this.setState({round: 1})
+  }
+  prevRound() {
+    console.log("clicked prev")
+    if (this.state.round === 1) {
+      console.log("lowest round (1)")
+      this.setState({round: 1})
+      return
+    }
+    this.setState({round: this.state.round - 1})
+  }
+  nextRound() {
+    console.log("clicked next")
+    if (this.state.round === 5) {
+      console.log("highest round (5)")
+      return 
+    }
+    this.setState({round: this.state.round + 1})
   }
   render() {
     const answer1 = 'sample answer'
@@ -13,7 +30,16 @@ export default class extends React.Component {
     const answer5 = 'sample answer 5'
     return (
       <div className="container-fluid">
-        <p className="sub-head">{this.state.round}</p>
+        <div className="round">
+          <i 
+            onClick={this.prevRound.bind(this)}
+            id="prev"
+            className="fa fa-angle-left"/> <p className="p-round">Round: {this.state.round} </p>
+          <i 
+            onClick={this.nextRound.bind(this)}
+            id="next"
+            className="fa fa-angle-right"/>
+          </div>
         <div className="row">
           <Square answer={answer1} clicked={false} cellNum={1} surveyNum={666}/>
         </div>
