e990f5973a14e80af29a140d007769d4e6fa4003
archae0pteryx <>
2017-03-20 15:37:04 -0700

    trucking along. click event in

--- src/components/board.jsx
+++ src/components/board.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
-import Square from './square'
+import React from "react";
+import Square from "./square";
+import {Data} from "../data";
 export default class extends React.Component {
+  componentWillMount() {
+    this.setState({
+      answers: Data.answers
+    })
+  }
   render() {
+    console.log(this.state.answers)
     return (
-      <Square/>
-    )
+      <div className="col">
+        {this.state.answers.map((text)=>{
+          return <Square key={text.id} answer={text.ans} clicked={false}/>
+        })}
+      </div>
+    );
   }
 }
--- src/components/square.jsx
+++ src/components/square.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
 export default class extends React.Component {
+  componentWillMount() {
+    this.setState({
+      clicked: false
+    })
+  }
+  handleClick() {
+    this.setState({clicked: !this.state.clicked})
+  }
   render() {
+    if (!this.state.clicked) {
+      return (
+        <div className="row">
+          <div className="col">
+            <div className="cell" onClick={this.handleClick.bind(this)}>
+              <h3>?</h3>
+            </div>
+          </div>
+        </div>
+      )
+    }
     return (
         <div className="row">
-          <h3>Square</h3>
+          <div className="col">
+            <div className="cell">
+            <h3>{this.props.answer}</h3>
+            </div>
+          </div>
       </div>
     )
   }
